84f0844880855120301b7cae59502fa8dacfe5e6
CryptoGrampy <cryptogrampy@protonmail.com>
2023-05-24 19:30:54 -0500

    Disable exchange tab

---
+++ env_config.js
@@ -0,0 +1,5 @@
+// Setting values here for local development.  This will be replaced by Docker entrypoint script.  
+window._env_ = {
+    MYMONERO_WEB_SERVER_URL: 'localhost',
+    MYMONERO_WEB_NETTYPE: 0
+}
