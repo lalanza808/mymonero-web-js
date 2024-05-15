ac4a7a359dc71e5d2cecfa47e26ebe3c02510933
lza_menace <lza_menace@protonmail.com>
2024-05-14 17:31:33 -0700

    update for running in local compose stack

--- env_config.js
+++ env_config.js
@@ -1,5 +1,5 @@
 // Setting values here for local development.  This will be replaced by Docker entrypoint script.  
 window._env_ = {
-    MYMONERO_WEB_SERVER_URL: 'localhost',
+    MYMONERO_WEB_SERVER_URL: 'http://localhost:8080/',
     MYMONERO_WEB_NETTYPE: 0
 }
